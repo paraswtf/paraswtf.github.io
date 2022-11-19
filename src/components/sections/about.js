009a7f9a8e5aed7afeee6f17ccf9c50ccd12074f
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-19 19:08:33 +0530

    update content in about and footer github

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -153,7 +153,12 @@ const About = () => {
               <a href="https://tunebot.org/">tunebot.org</a>,{' '}
               <a href="https://www.instagram.com/challengefitness007/">Challenge Fitness</a>, and{' '}
               <a href="https://www.instagram.com/am._designs/">AM Designs</a>, a small art business.
-              Currently I an working on rebranding .
+            </p>
+            <p>
+              Soon this page will host the website for{' '}
+              <a href="https://instagram.com/styxo.codes">styxo.codes</a>
+              and this portfolio will be moved somewhere else, because of the entire rebranding
+              thing, hopefully it doesn't become really confusing :/
             </p>
 
             <p>
