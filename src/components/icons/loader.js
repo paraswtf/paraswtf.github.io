ae87c82bfaf36134f0ea2d1f6078ae2f1769b5c8
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-19 18:14:16 +0530

    Initial commit

---
+++ src/components/icons/loader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const IconLoader = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="P" transform="translate(11.000000, 5.000000)">
+        <path
+          d="M44.54,31.5a10.62,10.62,0,0,1,5.27,1.27,8.93,8.93,0,0,1,3.53,3.6,11.22,11.22,0,0,1,1.25,5.38,11.15,11.15,0,0,1-1.25,5.37,8.88,8.88,0,0,1-3.53,3.58A10.73,10.73,0,0,1,44.54,52H34V47.5H44.7a4.94,4.94,0,0,0,2.67-.71,4.81,4.81,0,0,0,1.78-2,7.58,7.58,0,0,0,0-6.05,4.86,4.86,0,0,0-1.78-2A4.94,4.94,0,0,0,44.7,36H36.28v29H31.59V31.5h13Z"
+          transform="translate(-2.5 -2.42)"
+          fill="currentColor"
+        />
+      </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
+    </g>
+  </svg>
+);
+
+export default IconLoader;
---
+++ src/components/sections/about.js
@@ -0,0 +1,191 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      /*mix-blend-mode: multiply;*/
+      /*filter: grayscale(100%) contrast(1);*/
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'NextJS', 'Node.js', 'WordPress'];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hello! My name is Paras and I like creating cool things to solve unique problems. My
+              interest in web development started back in 2018 when I decided to create{' '}
+              <a href="https://github.com/paraswtf/Ririchiyo">Ririchiyo</a>, an integration for
+              Discord that let users play music and store playlists — like any sane person, I used
+              to bother every developer I found on online communities with the stupidest problems
+              while working on it, but I guess that's how I learnt!
+            </p>
+            <p>
+              During the pandemic in 2020, we had a lot of students from our school on discord
+              studying together, they helped me a lot in testing and refining the music integration
+              on discord.
+            </p>
+
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working for{' '}
+              <a href="https://tunebot.org/">tunebot.org</a>,{' '}
+              <a href="https://www.instagram.com/challengefitness007/">Challenge Fitness</a>, and{' '}
+              <a href="https://www.instagram.com/am._designs/">AM Designs</a>, a small art business.
+              Currently I an working on rebranding .
+            </p>
+
+            <p>
+              I have also recently been working on a{' '}
+              <a href="https://s.styxo.codes/">URL shortner</a> that lets people shorten their
+              links, create custom links, create profile pages with links and get analytics data.
+              It's still a work in progress, but you can still check it out :)
+            </p>
+
+            <p>Here are a few technologies I’ve been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
