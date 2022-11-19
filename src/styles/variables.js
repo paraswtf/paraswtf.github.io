ace403d1fd462afc53ad6273e7908566438addd8
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-19 19:56:40 +0530

    Fix theme

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,17 +2,17 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
-    --navy-shadow: rgba(2, 12, 27, 0.7);
+    --dark-navy: #021a27;
+    --navy: #021a27;
+    --light-navy: #023047;
+    --lightest-navy: #04374f;
+    --navy-shadow: rgba(1, 24, 36, 0.7);
     --dark-slate: #495670;
     --slate: #8892b0;
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #ff7c5e;
+    --green: #26d4ff;
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
